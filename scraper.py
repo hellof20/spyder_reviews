7dd915b68bf779c61985fc9d199ec9aff24d0f62
Pan, Wen-Ming <372476388@qq.com>
2020-09-29 22:44:26 +0800

    20200929

--- scraper.py
+++ scraper.py
@@ -157,12 +157,71 @@ def main():
     rdsuser = os.environ.get('rdsuser')
     rdspassword = os.environ.get('rdspassword')
     database = os.environ.get('rdsdatabase')
-    connect = create_engine('mysql+pymysql://' + rdsuser + ':' + rdspassword + '@' + rdshost + ':3306/' + database + '?charset=utf8')
+    # connect = create_engine('mysql+pymysql://' + rdsuser + ':' + rdspassword + '@' + rdshost + ':3306/' + database + '?charset=utf8')
+    connect = create_engine('mysql+pymysql://' + rdsuser + ':' + rdspassword + '@' + rdshost + ':3306?charset=utf8')
+    
+    #创建数据库和表
+    cursor = conn.cursor()
+    dbs = cursor.execute("show databases;")
+    print(dbs)
+
+    sql = """
+CREATE DATABASE spyder DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci;
+DROP TABLE IF EXISTS customer_ratings;
+create table customer_ratings(
+    appname varchar(128),
+    country varchar(16),
+    platform varchar(16),
+    date DATETIME,
+    totalNumberOfReviews int,
+    ratingAverage double,
+    ratingCount int,
+    1stars int,
+    2stars int,
+    3stars int,
+    4starts int,
+    5stars int)
+    ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS customer_reviews_temp;
+create table customer_reviews_temp(
+    id VARCHAR(128),
+    appname VARCHAR(128),
+    country VARCHAR(16),
+    platform varchar(16),
+    date DATETIME,
+    name VARCHAR(128),
+    title TEXT,
+    content TEXT,
+    rating smallint,
+    PRIMARY KEY ( id )) 
+    ENGINE=InnoDB  DEFAULT CHARSET=utf8;
+DROP TABLE IF EXISTS customer_reviews;
+create table customer_reviews(
+    id VARCHAR(128),
+    appname VARCHAR(128),
+    country VARCHAR(16),
+    platform varchar(16),
+    date DATETIME,
+    name VARCHAR(128),
+    title TEXT,
+    content TEXT,
+    rating smallint,
+    sentiment VARCHAR(500),
+    keyword VARCHAR(1000),
+    noun VARCHAR(500),
+    adj VARCHAR(500),
+    verb VARCHAR(500),
+    entity VARCHAR(500),
+    createtime DATETIME default CURRENT_TIMESTAMP,
+    updatetime DATETIME default CURRENT_TIMESTAMP,
+    PRIMARY KEY ( id ))
+    ENGINE=InnoDB  DEFAULT CHARSET=utf8 ;
+"""
+
+    #处理app.csv文件
     s3 = boto3.resource('s3')
     appbucket = os.environ.get('appbucket')
     appkey = os.environ.get('appkey')
-    print(appbucket)
-    print(appkey)
     s3.meta.client.download_file(appbucket, appkey, 'app.csv')
     game_df = pd.read_csv('app.csv')
     game_num = game_df.shape[0]
