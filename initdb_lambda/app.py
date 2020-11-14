6baa7fe7e9fa0cb2eaf3c5b425bb001b2451b264
Pan, Wen-Ming <372476388@qq.com>
2020-11-14 14:41:56 +0800

    20201114

--- initdb_lambda/app.py
+++ initdb_lambda/app.py
@@ -17,20 +17,19 @@ handler = cfn_resource.Resource()
 def create_thing(event, context):
     try:
         conn = pymysql.connect(rds_host, user=name, passwd=password, db=db_name, connect_timeout=5)
+        logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
+        with conn.cursor() as cur:
+            cur.execute("alter database "+ db_name + " CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" )
+            cur.execute("create table customer_ratings(appname varchar(128),country varchar(16),platform varchar(16),date DATETIME,totalNumberOfReviews int,ratingAverage double,ratingCount int,1stars int,2stars int,3stars int,4starts int,5stars int) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;")
+            cur.execute("create table customer_reviews_temp(id VARCHAR(128),appname VARCHAR(128),country VARCHAR(16),platform varchar(16),date DATETIME,name VARCHAR(128),title TEXT,content TEXT,rating smallint,PRIMARY KEY ( id,platform )) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;")
+            cur.execute("create table customer_reviews(id VARCHAR(128),appname VARCHAR(128),country VARCHAR(16),platform varchar(16),date DATETIME,name VARCHAR(128),title TEXT,content TEXT,rating smallint,sentiment VARCHAR(500),keyword VARCHAR(1000),noun VARCHAR(500),adj VARCHAR(500),verb VARCHAR(500),entity VARCHAR(500),createtime DATETIME default CURRENT_TIMESTAMP,updatetime DATETIME default CURRENT_TIMESTAMP,PRIMARY KEY ( id,platform ))ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;")
+            conn.commit()
+        logger.info("SUCCESS: Added tables succeeded")
+        return {"PhysicalResourceId": "arn:aws:fake:myID"}
     except pymysql.MySQLError as e:
         logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
         logger.error(e)
         sys.exit()
-    logger.info("SUCCESS: Connection to RDS MySQL instance succeeded")
-
-    with conn.cursor() as cur:
-        cur.execute("alter database "+ db_name + " CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci;" )
-        cur.execute("create table customer_ratings(appname varchar(128),country varchar(16),platform varchar(16),date DATETIME,totalNumberOfReviews int,ratingAverage double,ratingCount int,1stars int,2stars int,3stars int,4starts int,5stars int) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;")
-        cur.execute("create table customer_reviews_temp(id VARCHAR(128),appname VARCHAR(128),country VARCHAR(16),platform varchar(16),date DATETIME,name VARCHAR(128),title TEXT,content TEXT,rating smallint,PRIMARY KEY ( id,platform )) ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;")
-        cur.execute("create table customer_reviews(id VARCHAR(128),appname VARCHAR(128),country VARCHAR(16),platform varchar(16),date DATETIME,name VARCHAR(128),title TEXT,content TEXT,rating smallint,sentiment VARCHAR(500),keyword VARCHAR(1000),noun VARCHAR(500),adj VARCHAR(500),verb VARCHAR(500),entity VARCHAR(500),createtime DATETIME default CURRENT_TIMESTAMP,updatetime DATETIME default CURRENT_TIMESTAMP,PRIMARY KEY ( id,platform ))ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;")
-        conn.commit()
-    logger.info("SUCCESS: Added tables succeeded")
-    return {"PhysicalResourceId": "arn:aws:fake:myID"}
 
 @handler.update
 def update_thing(event, context):
