62c5ec724fdc30249e773beba5cd7ad515a2c3d2
Pan, Wen-Ming <smalljelly2018@gmail.com>
2021-06-21 21:01:36 +0800

    Update app.py

--- initdb_lambda/app.py
+++ initdb_lambda/app.py
@@ -3,6 +3,7 @@ import logging
 import os
 import pymysql
 import cfn_resource
+import time
 
 rds_host  = os.environ['db_endpoint']
 name = os.environ['db_username']
@@ -25,6 +26,7 @@ def create_thing(event, context):
             cur.execute("create table customer_reviews(id VARCHAR(128),appname VARCHAR(128),country VARCHAR(16),platform varchar(16),date DATETIME,name VARCHAR(128),title TEXT,content TEXT,rating smallint,sentiment VARCHAR(500),keyword VARCHAR(1000),noun VARCHAR(500),adj VARCHAR(500),verb VARCHAR(500),entity VARCHAR(500),createtime DATETIME default CURRENT_TIMESTAMP,updatetime DATETIME default CURRENT_TIMESTAMP,PRIMARY KEY ( id,platform ))ENGINE=InnoDB  DEFAULT CHARSET=utf8mb4 COLLATE utf8mb4_unicode_ci;")
             conn.commit()
         logger.info("SUCCESS: Added tables succeeded")
+        time.sleep(10)
         return {"PhysicalResourceId": "arn:aws:fake:myID"}
     except pymysql.MySQLError as e:
         logger.error("ERROR: Unexpected error: Could not connect to MySQL instance.")
